6f4f9064954730fd48beaae5f73bd2151a446cd3
xomyachok-shaolin <56404900+xomyachok-shaolin@users.noreply.github.com>
2021-12-02 17:07:12 +0500

    Update httpCommons.js

--- src/httpCommons.js
+++ src/httpCommons.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-  baseURL: `http://localhost:8090`,
-  headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:4000'
-  }
+  baseURL: `https://stp-hotel.herokuapp.com/`
 })
